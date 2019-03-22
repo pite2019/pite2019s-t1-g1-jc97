c83ddf66a96221668c6d9d070f6d7c408e2245d1
Julian Knorr <julian.knorr@ruhr-uni-bochum.de>
2019-03-22 12:49:49 +0100

    indentation

--- matrix.py
+++ matrix.py
@@ -4,35 +4,35 @@ from typing import List
 
 
 class Matrix:
-    __lines = ... # type: int
-    __columns = ... # type: int
-    __matrix = ... # type: List[List[int]]
+	__lines = ... # type: int
+	__columns = ... # type: int
+	__matrix = ... # type: List[List[int]]
 
-    def __init__(self, lines, columns):
-        if not isinstance(lines, int) or lines < 2 or not isinstance(columns, int) or columns < 2:
-            raise ValueError()
-        self.__lines = lines
-        self.__columns = columns
-        self.__matrix = []
-        for i in range(0, columns):
-            self.__matrix[i] = []
-            for j in range(0, lines):
-                self.__matrix[i][j] = 0
+	def __init__(self, lines, columns):
+		if not isinstance(lines, int) or lines < 2 or not isinstance(columns, int) or columns < 2:
+			raise ValueError()
+		self.__lines = lines
+		self.__columns = columns
+		self.__matrix = []
+		for i in range(0, columns):
+			self.__matrix[i] = []
+			for j in range(0, lines):
+				self.__matrix[i][j] = 0
 
-    @property
-    def lines(self):
-        return 
+	@property
+	def lines(self):
+		return 
 
 
-    def setColumn(self, column, values):
-        if not isinstance(column, int) or column >= self.__columns:
-            raise ValueError()
-        if not isinstance(values, List) or len(values) != self.__lines:
-            raise ValueError()
-        self.__matrix[column] = values
+	def setColumn(self, column, values):
+		if not isinstance(column, int) or column >= self.__columns:
+			raise ValueError()
+		if not isinstance(values, List) or len(values) != self.__lines:
+			raise ValueError()
+		self.__matrix[column] = values
 
-    def setValues(self, values):
-        if not isinstance(values, List) or len(values) != self.__lines:
+	def setValues(self, values):
+		if not isinstance(values, List) or len(values) != self.__lines:
 			raise ValueError()
 		for line in values:
 			if not isinstance(line, List) or len(line) != self.__columns:

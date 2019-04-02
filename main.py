29bf18e48dd6eb129d0d0e727ded4d0af338b699
Julian Knorr <git@jknorr.eu>
2019-04-02 23:28:50 +0200

    PEP8

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from matrix import Matrix
 
-matrixA = Matrix(2, 2)
-matrixB = Matrix(2, 2)
+matrixA = Matrix(2, 3)
+matrixB = Matrix(2, 3)
 
-matrixA.setValues([[1, 2], [3, 4]])
-matrixB.setValues([[5, 6], [7, 8]])
+matrixA.set_values([[10, 20, 100], [30, 40.2, 110]])
+matrixB.set_values([[5, 6, 200], [7, 8, 300]])
 
 matrixC = matrixB - matrixA
 
-matrixC.printMatrix()
-print("");
-matrixC -= 3
-matrixC.printMatrix()
+matrixC.print_matrix()
+
+matrixC = 2 - matrixC
+matrixC.print_matrix()
+
+for line in matrixC:
+	print(line)
--- matrix.py
+++ matrix.py
@@ -4,9 +4,9 @@ from typing import List
 
 
 class Matrix:
-	__lines = ... # type: int
-	__columns = ... # type: int
-	__matrix = ... # type: List[List[int]]
+	__lines = ...  # type: int
+	__columns = ...  # type: int
+	__matrix = ...  # type: List[List[int]]
 
 	def __init__(self, lines, columns):
 		if not isinstance(lines, int) or lines < 2 or not isinstance(columns, int) or columns < 2:
@@ -14,46 +14,35 @@ class Matrix:
 		self.__lines = lines
 		self.__columns = columns
 		self.__matrix = []
-		for i in range(0, columns):
+		self.__num = 0
+		for i in range(0, lines):
 			self.__matrix.append([])
-			for j in range(0, lines):
+			for j in range(0, columns):
 				self.__matrix[i].append(0)
 
-	@property
-	def lines(self):
-		return 
-
-
-	def setColumn(self, column, values):
-		if not isinstance(column, int) or column >= self.__columns:
-			raise ValueError()
-		if not isinstance(values, List) or len(values) != self.__lines:
-			raise ValueError()
-		self.__matrix[column] = values
-
-	def setValues(self, values):
+	def set_values(self, values):
 		if not isinstance(values, List) or len(values) != self.__lines:
 			raise ValueError()
 		for line in values:
 			if not isinstance(line, List) or len(line) != self.__columns:
 				raise ValueError()
 			for value in line:
-				if not isinstance(value, int):
+				if not isinstance(value, int) and not isinstance(value, float):
 					raise ValueError()
 		self.__matrix = values
 
-	def addMatrix(self, summand):
+	def add_matrix(self, summand):
 		if not isinstance(summand, Matrix) or summand.__lines != self.__lines or summand.__columns != self.__columns:
 			raise ValueError()
-		for i in range(0, self.__columns):
-			for j in range(0, self.__lines):
+		for i in range(0, self.__lines):
+			for j in range(0, self.__columns):
 				self.__matrix[i][j] += summand.__matrix[i][j]
 
-	def substractMatrix(self, subtrahend):
+	def subtract_matrix(self, subtrahend):
 		if not isinstance(subtrahend, Matrix) or subtrahend.__lines != self.__lines or subtrahend.__columns != self.__columns:
 			raise ValueError()
-		for i in range(0, self.__columns):
-			for j in range(0, self.__lines):
+		for i in range(0, self.__lines):
+			for j in range(0, self.__columns):
 				self.__matrix[i][j] += subtrahend.__matrix[i][j]
 
 	def __add__(self, summand):
@@ -63,34 +52,49 @@ class Matrix:
 		elif not isinstance(summand, Matrix) or summand.__lines != self.__lines or summand.__columns != self.__columns:
 			raise ValueError()
 		result = Matrix(self.__lines, self.__columns)
-		for i in range(0, self.__columns):
-			for j in range(0, self.__lines):
+		for i in range(0, self.__lines):
+			for j in range(0, self.__columns):
 				if numeric_summand:
 					result.__matrix[i][j] = self.__matrix[i][j] + summand
 				else:
 					result.__matrix[i][j] = summand.__matrix[i][j] + self.__matrix[i][j]
 		return result
 
+	def __radd__(self, other):
+		return self.__add__(other)
 
-	def __sub__(self, subtrahend):
+	def __sub__(self, subtrahend, invert=False):
 		numeric_subtrahend = False
 		if isinstance(subtrahend, float) or isinstance(subtrahend, int):
 			numeric_subtrahend = True
-		elif not isinstance(subtrahend, Matrix) or subtrahend.__lines != self.__lines or subtrahend.__columns != self.__columns:
+		elif not isinstance(subtrahend, Matrix) or subtrahend.__lines != self.__lines \
+			or subtrahend.__columns != self.__columns:
 			raise ValueError()
 		result = Matrix(self.__lines, self.__columns)
-		for i in range(0, self.__columns):
-			for j in range(0, self.__lines):
+		for i in range(0, self.__lines):
+			for j in range(0, self.__columns):
 				if numeric_subtrahend:
-					result.__matrix[i][j] = self.__matrix[i][j] - subtrahend
+					result.__matrix[i][j] = (self.__matrix[i][j] - subtrahend) * (-1 if invert else 1)
 				else:
-					result.__matrix[i][j] = self.__matrix[i][j] - subtrahend.__matrix[i][j]
+					result.__matrix[i][j] = (self.__matrix[i][j] - subtrahend.__matrix[i][j]) * (-1 if invert else 1)
 		return result
-
 	
-	def printMatrix(self):
-		for j in range(0, self.__lines):
-			for i in range(0, self.__columns):
-				print(str(self.__matrix[i][j]) + " ", end = '')
-			print("\n", end = '')
+	def __rsub__(self, other):
+		return self.__sub__(other, True)
 
+	def print_matrix(self):
+		for i in range(0, self.__lines):
+			for j in range(0, self.__columns):
+				print(str(self.__matrix[i][j]) + " ", end='')
+			print("\n", end='')
+		print("")
+
+	def __iter__(self):
+		return self
+
+	def __next__(self):
+		if self.__num < self.__lines:
+			self.__num += 1
+			return self.__matrix[self.__num - 1]
+		else:
+			raise StopIteration()
